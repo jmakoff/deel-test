9e82e0d62e1725b454708a4f1d2485a7ff09bf55
Yurii Makov <y.makov@p1k.org>
2023-11-11 17:52:01 +0000

    finished jobs section

--- src/routes/jobs.js
+++ src/routes/jobs.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getProfile } = require("../middleware/getProfile");
 const router = express.Router();
+const { getProfile } = require("../middleware/getProfile");
 const { Op } = require("sequelize");
+const { sequelize } = require("../model");
 
 router.get("/unpaid", getProfile, async (req, res) => {
   const { Job, Contract } = req.app.get("models");
@@ -23,6 +24,64 @@ router.get("/unpaid", getProfile, async (req, res) => {
   }
 
   res.json(jobs);
+});
+
+router.post("/:job_id/pay", getProfile, async (req, res) => {
+  try {
+    await sequelize.transaction(async () => {
+      const { Job, Profile, Contract } = req.app.get("models");
+      const { job_id } = req.params;
+      const [profile, job] = await Promise.all([
+        Profile.findOne({
+          attributes: ["balance", "id"],
+          where: {
+            id: req.profile.id,
+          },
+        }),
+        Job.findOne({
+          include: [Contract],
+          attributes: ["price", "id", "paid"],
+          where: {
+            id: job_id,
+            "$Contract.ClientId$": req.profile.id,
+          },
+        }),
+      ]);
+
+      if (!profile || !job) {
+        const errorMsg = `Cannot find contract or client info`;
+        res.status(404);
+        res.send(errorMsg);
+        throw new Error(errorMsg);
+      }
+
+      if (job.paid) {
+        const errorMsg = `This job is already paid`;
+        res.status(403);
+        res.send(errorMsg);
+        throw new Error(errorMsg);
+      }
+
+      if (profile.balance < job.price) {
+        const errorMsg = `You have not enough money on balance`;
+        res.status(403);
+        res.send(errorMsg);
+        throw new Error(errorMsg);
+      }
+
+      profile.balance = profile.balance - job.price;
+      job.set({
+        paid: true,
+        paymentDate: new Date(),
+      });
+      await Promise.all([profile.save(), job.save()]);
+
+      return res.status(201).end;
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
 });
 
 module.exports = router;
